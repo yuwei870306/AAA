43243f20e8ddf6874437d76d3dabf8b3140a1855
于威 <yuwei870306@163.com>
2015-10-08 11:03:46 +0800

    Initial Commit

---
+++ RunTimeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunTimeDemo
+//
+//  Created by 于威 on 15/10/8.
+//  Copyright (c) 2015年 于威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RunTimeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RunTimeDemo
+//
+//  Created by 于威 on 15/10/8.
+//  Copyright (c) 2015年 于威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
